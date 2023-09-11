2e144fe7107bc6256c59e95ad706ff1283b6f3ee
azrayos01 <azrayos01@gmail.com>
2023-09-11 12:47:46 -0500

    first commit

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+'''
+Autor: Yazmin Rincón
+Fecha: 11 Sep 2023
+Descripción: Calculadora que suma
+'''
+
+a = input ("Ingresa el primer valor ")
+b = input ("Ingresa el segundo valor")
+print(type(a), type(b))
+print ("Suma ", a + b)
+a = int(a)
+b =int(b)
+print(type(a), type(b))
+print ("Suma ", a + b)
+a = float(a)
+b = float(b)
+print(type(a), type(b))
+print ("Resta ", a - b)
+a = float(a)
+b = float(b)
+print(type(a), type(b))
+print ("Multiplicación ", a * b)
+a = float(a)
+b = float(b)
+print(type(a), type(b))
+print ("División ", a / b)
+a = int(a)
+b = int(b)
