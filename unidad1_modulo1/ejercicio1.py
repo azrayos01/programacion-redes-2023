ed8a20cceea36cb9aad987abb48cc0d85bcd7439
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2023-09-25 11:17:34 -0600

    Add files via upload

---
+++ unidad1_modulo1/ejercicio1.py
@@ -0,0 +1,10 @@
+﻿"""
+Autor: Yazmin Elizabeth Rincón Ulloa 
+Fecha de creación: 25 de Septiembre del 2023
+unidad1_modulo1
+Ejercicio practico 1
+"""
+
+print(((3 + 2) / 2.5) ** 2)
+
+
---
+++ unidad1_modulo1/ejercicio2.py
@@ -0,0 +1,10 @@
+"""
+Autor: Yazmin Elizabeth Rincón Ulloa 
+Fecha de creación: 25 de Septiembre del 2023
+unidad1_modulo1
+Ejercicio practico 2
+"""
+
+n = int(input("Dame un numero:"))
+
+print(n * ((n + 1)/2))
