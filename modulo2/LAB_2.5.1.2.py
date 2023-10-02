d57a87d58e343998fa7bfab50b07bca78ad05043
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2023-10-01 18:40:32 -0600

    Add files via upload

---
+++ modulo2/LAB_2.5.1.2.py
@@ -0,0 +1,13 @@
+"""
+Autor: Yazmin Elizabeth Rincon Ulloa
+Descripcion: COMENTARIOS en un programa
+Fecha: Lunes 25 de Septiembre del 2023
+"""
+# este programa calcula los segundos en cierto número de horas determinadas 
+
+cant_hors = 2 
+seg_hora = 3600 
+
+print("Horas: ", cant_hors) 
+print("Segundos en Horas: ", cant_hors * seg_hora) 
+print('"Adios"')
---
+++ modulo2/LAB_2.6.1.9.py
@@ -0,0 +1,14 @@
+"""
+Autor: Yazmin Elizabeth Rincon Ulloa
+Descripcion: Familiarizarse con la entrada y salida de datos en Python, Evaluar expresiones simples.
+Fecha: Lunes 25 de Septiembre del 2023
+"""
+
+a = float(input("Valor para a: "))
+b = float(input("Valor para b: "))
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicacion:", a * b)
+print("Division:", a / b)
+print("\n¡Eso es todo, amigos!")
