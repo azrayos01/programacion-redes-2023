86e94ef05efae212fabe2ac57d23f402e198f949
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2023-10-01 19:06:09 -0600

    Add files via upload

---
+++ modulo3/LAB_3.1.1.2.py
@@ -0,0 +1,19 @@
+"""
+Autor: Yazmin Elizabeth Rincon Ulloa
+Descripcion: Tomando Desiciones
+Fecha: Domingo 01 de Octubre del 2023
+"""
+#3.1.1.2
+var = 0 
+print(var == 0)
+
+var = 1
+print(var == 0)
+
+var = 0 
+print(var != 0)
+
+var = 1
+print(var != 0)
+
+#
