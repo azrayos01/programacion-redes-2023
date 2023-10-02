3be09e07e0972112c826c2730b7df5c04bd5b5f6
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2023-10-02 10:19:44 -0600

    Add files via upload

--- modulo2/LAB_2.1.1.19.py
+++ modulo2/LAB_2.1.1.19.py
@@ -1,1 +1,42 @@
+"""
+Autor: Yazmin Elizabeth Rincon Ulloa
+Descripcion: Familiarizarse con la funcion print()
+Fecha: Lunes 02 de Octubre del 2023
+"""
 
+print( "    *" )
+print( "   * *" )
+print( "  *   *" )
+print( " *     *" )
+print( "***   ***" )
+print( "  *   *" )
+print( "  *   *" )
+print( "  *****" )
+
+print( "    *     " * 2 )
+print( "   * *    " * 2 )
+print( "  *   *   " * 2 )
+print( " *     *  " * 2 )
+print( "***   *** " * 2 )
+print( "  *   *   " * 2 )
+print( "  *   *   " * 2 )
+print( "  *****   " * 2 )
+
+print("    \n   * *\n  *   *\n *     *\n**   ***\n  *   *\n  *   *\n  *****")
+
+print( "    *" )
+print( "    *" )
+print( "   * *" )
+print( "   * *" )
+print( "  *   *" )
+print( "  *   *" )
+print( " *     *" )
+print( " *     *" ) 
+print( "***   ***" )
+print( "***   ***" )
+print( "  *   *" )
+print( "  *   *" )
+print( "  *   *" )
+print( "  *   *" )
+print( "  *****" )
+print( "  *****" )
