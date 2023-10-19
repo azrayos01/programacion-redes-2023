c3d78e2052a14b1baa0493b2d960986cf478abbf
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2023-10-19 17:16:57 -0600

    Add files via upload

---
+++ Recuperacion01/ejercicio.py
@@ -0,0 +1,14 @@
+""""
+Nombre: Yazmin Elizabeth Roncon Ulloa
+Asignatura: Programacion de Redes
+Descripcion: Clonar repositorio de la asignatura en tu máquina local o extraer los datos de la nube.
+
+"""
+
+asignatura = []
+array = []
+
+for i in asignatura:
+    asignatura = int(input("Dame las calificaciones de tus unidades: "))
+   # array = uniTem
+print("Unidades tematicas:", array)
