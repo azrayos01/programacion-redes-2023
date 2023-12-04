d90fb10c2a227fb2d2203bc6f37a385593a69b42
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2023-12-04 10:33:25 -0600

    Update lab22_yazmin.py

--- UNIDAD_3/lab22_yazmin.py
+++ UNIDAD_3/lab22_yazmin.py
@@ -1,18 +1,1 @@
-"""
-Conexion
-Autor: Rincon Ulloa Yazmin Elizabeth
-Fecha; 24 de Noviembre del 2023
-"""
 
-from netmiko import ConnectHandler
-
-sshCli = ConnectHandler(
-    device_type='cisco_ios',
-    host='10.10.20.48',
-    port=22,
-    username = 'developer',
-    password = 'C1sco12345'
-)
-
-salida = sshCli.send_command('show ip int brief')
-print(salida)
