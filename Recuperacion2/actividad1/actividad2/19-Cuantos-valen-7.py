3ccd0902ce6767f45ed0f2cc7ac038e207198276
azrayos01 <142462490+azrayos01@users.noreply.github.com>
2024-01-15 19:10:38 -0600

    Add files via upload

---
+++ Recuperacion2/actividad1/actividad2/19-Cuantos-valen-7.py
@@ -0,0 +1,5 @@
+
+num1 = int(input())
+num2 = int(input())
+cantidad_siete = (num1 == 7) + (num2 == 7)
+print(cantidad_siete)
---
+++ Recuperacion2/actividad1/actividad2/23-Tazon-de-Fruta.py
@@ -0,0 +1,8 @@
+"""
+Autor: Rincon Ulloa Yazmin Elizabeth
+Fecha: 15/01/2024
+Descripcion: Tazon de Fruta
+"""
+cantidad_total_fruta = int(input())
+pays_de_mango = (cantidad_total_fruta // 3) //2
+print(pays_de_mango)
